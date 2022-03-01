ac54e77e134c46bc9a3f93cf791da1bf832df19a
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-01 23:08:33 +0700

    add: ejs setup

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.listen(process.env.PORT || 8000);
