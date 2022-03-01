4bdb7ce4710c7ca4e480f0d5e491799b06308b00
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-01 23:55:50 +0700

    feat: post shortener

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import UrlShortener from './models/urlShortener.js'
 
 mongoose.connect('mongodb://localhost/urlShortener', {
     useNewUrlParser: true,
@@ -9,9 +10,15 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.post('/shortener', async (req, res) =>{
+   await UrlShortener.create({ full: req.body.fullUrl });
+   res.redirect('/');
 })
 
 app.listen(process.env.PORT || 8000);
