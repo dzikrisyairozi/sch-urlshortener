d96026b69c68ffdc4940ce38a96e1525a8b35054
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-02 00:04:11 +0700

    feat: dynamic url shortener input

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls = await UrlShortener.find();
+    res.render(
+        'index',
+        { shortUrls: shortUrls }
+    );
 })
 
 app.post('/shortener', async (req, res) =>{
