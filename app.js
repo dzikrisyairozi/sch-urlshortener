0f8499a02671139ed488c7e059911f265ddbc6b8
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-02 20:27:22 +0700

    feat: homeRoutes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import homeRoutes from './routes/home.js'
 import UrlShortener from './models/urlShortener.js'
 
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -12,33 +13,11 @@ const PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
-
-app.get('/', async (req, res) => {
-    const shortUrls = await UrlShortener.find();
-    res.render(
-        'index',
-        { shortUrls: shortUrls }
-    );
-})
+app.use('/', homeRoutes);
 
 app.post('/shortener', async (req, res) =>{
    await UrlShortener.create({ fullUrl: req.body.fullUrl });
    res.redirect('/');
-})
-
-app.get('/:shortUrl', async (req, res)=>{
-    const shortUrl = await UrlShortener.findOne({ shortUrl: req.params.shortUrl })
-    if ( shortUrl == null ) {
-        return res.sendStatus(404).json({
-            success: false,
-            msg : 'URL nor found!'
-        });
-    }
-
-    shortUrl.clicks++;
-    shortUrl.save();
-
-    res.redirect(shortUrl.fullUrl);
 })
 
 const start =  (req, res) => {
