7e04543e623669d210f1595bf5f6f3ac214aeee9
dikiogres <dikiogres@gmail.com>
2022-03-08 20:31:52 +0700

    feat: auth route in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import homeRoutes from './routes/home.js'
 import shortenerRoutes from './routes/shortener.js'
+import authRoutes from './routes/auth.js'
 
 mongoose.connect('mongodb://localhost/urlShortener', {
     useNewUrlParser: true,
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use('/', homeRoutes);
 app.use('/shortener', shortenerRoutes);
+app.use(authRoutes);
 
 const start =  (req, res) => {
     try{
