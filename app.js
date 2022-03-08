c43beaf841dee7bfce85a536f3495c64919ed104
dikiogres <dikiogres@gmail.com>
2022-03-08 21:27:08 +0700

    feat: cookies primer using cookie-parser package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser'
 import homeRoutes from './routes/home.js'
 import shortenerRoutes from './routes/shortener.js'
 import authRoutes from './routes/auth.js'
@@ -17,6 +18,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', homeRoutes);
 app.use('/shortener', shortenerRoutes);
 app.use('/auth', authRoutes);
+app.use(cookieParser());
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('mamam nih cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
 
 const start =  (req, res) => {
     try{
