269e9b4475ff730f74654ecf38ffdcca970bfed7
dikiogres <dikiogres@gmail.com>
2022-03-08 20:29:58 +0700

    feat: auth controller setup

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+export const signup_get = (req, res) => {
+    res.render('signup');
+}
+  
+  export const login_get = (req, res) => {
+    res.render('login');
+}
+  
+  export const signup_post = async (req, res) => {
+    res.send('new signup');
+}
+  
+  export const login_post = async (req, res) => {
+    res.send('user login');
+}
+
+export default {
+    signup_get,
+    login_get,
+    signup_post,
+    login_post
+}
