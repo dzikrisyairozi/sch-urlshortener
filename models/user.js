89fd90e878f97f14b64f7e0a982a2ddfc1b06307
dikiogres <dikiogres@gmail.com>
2022-03-08 20:41:51 +0700

    add: user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  }
+});
+
+export default mongoose.model('User', userSchema);
+
