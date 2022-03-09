4429bbee9b87875d2b567b62a87cb921bbd05b0b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-09 09:50:21 +0700

    feat: update login post controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,8 +59,12 @@ export const signup_get = (req, res) => {
   export const login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password);
-    res.send('user login');
+    try {
+      const user = await User.login(email, password);
+      res.status(200).json({ user: user._id });
+    } catch (err) {
+      res.status(400).json({});
+    }
 }
 
 export default {
