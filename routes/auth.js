6af8457444170e5c90bf9816f6ddd5ece3cbd6fa
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-09 11:35:54 +0700

    feat: logout route

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,6 @@ router.get('/signup', auth.signup_get);
 router.post('/signup', auth.signup_post);
 router.get('/login', auth.login_get);
 router.post('/login', auth.login_post);
+router.get('/logout', authController.logout_get);
 
 export default router;
