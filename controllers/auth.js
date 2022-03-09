459e459ec0ab3b593dac81ead8c95b1ed951eb65
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-09 09:37:40 +0700

    fix: ketinggalan

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ export const signup_get = (req, res) => {
         const user = await User.create({ email, password });
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json(user);
+        res.status(201).json( { user: user._id });
       }
       catch(err) {
         const errors = handleErrors(err);
