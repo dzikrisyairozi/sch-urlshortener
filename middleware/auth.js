2dcead999282d3ca0e0a4a32271f60fbfe8bdd87
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-10 20:22:52 +0700

    feat: auth middleware try and catch

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,8 @@ import User from '../models/user.js'
 
 export const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
-
-  // check json web token exists & is verified
+  try{
+      // check json web token exists & is verified
   if (token) {
     jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
       if (err) {
@@ -17,28 +17,37 @@ export const requireAuth = (req, res, next) => {
   } else {
     res.redirect('/auth/login');
   }
+  }
+  catch(err) {
+    return res.send(err);
+  }
 };
 
 // check current user
 export const checkUser = (req, res, next) => {
     const token = req.cookies.jwt;
-    if (token) {
-      jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
-        if (err) {
-          res.locals.user = null;
-          // console.log("iz null bois");
-          next();
-        } else {
-          let user = await User.findById(decodedToken.id);
-          res.locals.user = user;
-          req.user = user;
-          next();
-        }
-      });
-    } else {
-      res.locals.user = null;
-      // console.log("iz null bois");
-      next();
+    try{
+      if (token) {
+        jwt.verify(token, 'schematics url shortener', async (err, decodedToken) => {
+          if (err) {
+            res.locals.user = null;
+            // console.log("iz null bois");
+            next();
+          } else {
+            let user = await User.findById(decodedToken.id);
+            res.locals.user = user;
+            req.user = user;
+            next();
+          }
+        });
+      } else {
+        res.locals.user = null;
+        // console.log("iz null bois");
+        next();
+      }
+    }
+    catch(err) {
+      return res.send(err);
     }
   };
 
