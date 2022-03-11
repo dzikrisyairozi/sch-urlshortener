8be3bd5dc5786fd2183ba7b2bcfec9300de40f3b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-11 15:17:25 +0700

    refactor: auth route

--- routes/home.js
+++ routes/home.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.route("/").get(home.getHome);
 router.route("/dashboard").get(dashboardController.homeGetDashboard);
-router.route("/auth/login").get(authController.login_get);
-router.route("/auth/signup").get(authController.signup_get);
+router.route("/login").get(authController.login_get);
+router.route("/signup").get(authController.signup_get);
 router.route("/:shortUrl").get(home.getShortUrl);
 
 export default router;
