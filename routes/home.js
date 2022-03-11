8c9736b914d10bab200d93c24f7cf57ef1ad51ed
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-03-11 16:04:56 +0700

    fix:logout routes

--- routes/home.js
+++ routes/home.js
@@ -9,6 +9,7 @@ router.route("/").get(home.getHome);
 router.route("/dashboard").get(dashboardController.homeGetDashboard);
 router.route("/login").get(authController.login_get);
 router.route("/signup").get(authController.signup_get);
+router.route("/logout").get(authController.logout_get);
 router.route("/:shortUrl").get(home.getShortUrl);
 
 export default router;
